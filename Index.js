cb6f5a11c1192232e89633821919f22ef3c9005f
HARSH TIWARI <111190169+harshtiwariexe@users.noreply.github.com>
2023-05-12 01:19:36 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -4,33 +4,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-////////  FILES  //////////
 
-// Blocking Synchronus Way
-/* const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(textIn);
-
-const textOut = `This is what we know about Avacado: ${textIn}. \nCreated on ${Date.now()} `
-fs.writeFileSync('./txt/output.txt',textOut)
-console.log("File has been written and updated");
-
-const textOp = fs.readFileSync('./txt/output.txt','utf-8');
-console.log(textOp); */
-
-// Non-Blocking Asynchronus Way
-/*fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
-    fs.readFile(`./txt/${data1}`,'utf-8',(err,data2)=>{
-        console.log(_data2);
-        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
-            console.log(data3);
-
-            fs.writeFile('./txt/final.text',`${data1}\n${data3}`,'utf-8',err =>{
-                console.log(`Your file has been written`);
-            })
-        })
-    })
-})
-console.log('Reading File.....'); */
 
 ///////// SERVER ///////////
 const replaceTemp = (temp, product) => {
